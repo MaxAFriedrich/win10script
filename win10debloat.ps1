c0e2febc8c6134d5046bd19940b60da50649c17d
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2021-10-02 19:05:59 +0100

    fiex url

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -20,7 +20,7 @@ Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTI
 Write-Host "Running O&O Shutup with Recommended Settings"
 
 Import-Module BitsTransfer
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/ChrisTitusTech/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/MaxAFriedrich/win10script/master/ooshutup10.cfg" -Destination ooshutup10.cfg
 Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
 ./OOSU10.exe ooshutup10.cfg /quiet
 
